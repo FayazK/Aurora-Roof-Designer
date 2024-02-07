04c0a470baf9aa6a7ec07a87499e67f3a367f8dc
Fayaz K <info@fayazk.com>
2024-02-07 18:41:02 +0500

    init app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Aurora 3D Roof Designer
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Layout} from "antd";
+import Designer from "./Designer";
+
+
+const {Header, Content, Footer, Sider} = Layout;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (<Layout>
+        <Header
+            style={{
+                height:'60px',
+                display: 'flex', alignItems: 'center',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            Header Toolbar
+        </Header>
+        <Content>
+            <Designer />
+        </Content>
+    </Layout>);
 }
 
 export default App;
