3ce93226b2411497334421538f9880d0127e1285
Fayaz K <info@fayazk.com>
2024-03-01 17:13:11 +0500

    🔧 fix(Designer.jsx): wrap components in OrthographicCamera to enable 2D view and improve user experience 🔧 fix(DesignerOrbitControls.jsx): add GizmoHelper and GizmoViewport components to provide visual cues for rotation in the designer view

--- src/Components/Designer.jsx
+++ src/Components/Designer.jsx
@@ -4,14 +4,17 @@ import {MapTile} from "./Designer/MapTile.jsx";
 import {SceneLighting} from "./Designer/CanvasComponents.jsx";
 import {cameraProps, glProps} from "../helpers/global_props.js";
 import {DesignerOrbitControls} from "./Designer/DesignerOrbitControls.jsx";
+import {OrbitControls, OrthographicCamera} from "@react-three/drei";
 
 
 export default function Designer() {
 
     return (<Canvas camera={cameraProps} gl={glProps}>
-        <DynamicDrawing/>
-        <MapTile imageUrl={'staticmap.png'}/>
-        <SceneLighting/>
-        <DesignerOrbitControls/>
+        <OrthographicCamera>
+            <DynamicDrawing/>
+            <MapTile imageUrl={'staticmap.png'}/>
+            <SceneLighting/>
+            <DesignerOrbitControls/>
+        </OrthographicCamera>
     </Canvas>)
 }// Designer
--- src/Components/Designer/DesignerOrbitControls.jsx
+++ src/Components/Designer/DesignerOrbitControls.jsx
@@ -1,7 +1,15 @@
-import {OrbitControls, OrthographicCamera} from "@react-three/drei";
+import {GizmoHelper, GizmoViewport, OrbitControls} from "@react-three/drei";
 import {memo} from "react";
 
 export const DesignerOrbitControls = ()=>{
-    return <OrbitControls />
-    /*return <OrbitControls enableDamping={false} />*/
+    return <>
+        <GizmoHelper
+            alignment="bottom-right" // widget alignment within scene
+            margin={[80, 80]} // widget margins (X, Y)
+            rotation={true} // enable rotation
+        >
+            <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+        </GizmoHelper>
+        <OrbitControls />
+    </>
 }
