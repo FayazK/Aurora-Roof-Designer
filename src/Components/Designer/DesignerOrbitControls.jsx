01c268a6dcf75c24bcecff43519692e91662eaef
Fayaz K <info@fayazk.com>
2024-03-05 11:01:57 +0500

    ✨ (DesignerOrbitControls.jsx): add restrictions to OrbitControls to improve user experience and prevent unwanted camera movements

--- src/Components/Designer/DesignerOrbitControls.jsx
+++ src/Components/Designer/DesignerOrbitControls.jsx
@@ -1,15 +1,19 @@
 import {GizmoHelper, GizmoViewport, OrbitControls} from "@react-three/drei";
 import {memo} from "react";
 
-export const DesignerOrbitControls = ()=>{
+export const DesignerOrbitControls = () => {
     return <>
         <GizmoHelper
             alignment="bottom-right" // widget alignment within scene
             margin={[80, 80]} // widget margins (X, Y)
             rotation={true} // enable rotation
         >
-            <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+            <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black"/>
         </GizmoHelper>
-        <OrbitControls />
+        <OrbitControls
+            minPolarAngle={Math.PI / 2 + 0.1} // Restrict top view
+            minAzimuthAngle={0} // Disable tilting to the left
+            maxAzimuthAngle={0} // Disable tilting to the right
+        />
     </>
 }
