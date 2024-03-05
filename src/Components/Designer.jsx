a229e730b7f16a6be0d61d9e4d51344e90c00252
Fayaz K <info@fayazk.com>
2024-03-05 15:43:15 +0500

    📝 (Designer.jsx): remove unused import of DesignerOrbitControls to improve code cleanliness and reduce unused dependencies

--- src/Components/Designer.jsx
+++ src/Components/Designer.jsx
@@ -3,19 +3,16 @@ import DynamicDrawing from "./Designer/DynamicDrawing.jsx";
 import {MapTile} from "./Designer/MapTile.jsx";
 import {SceneLighting} from "./Designer/CanvasComponents.jsx";
 import {cameraProps, glProps} from "../helpers/global_props.js";
-import {DesignerOrbitControls} from "./Designer/DesignerOrbitControls.jsx";
 import {OrbitControls, OrthographicCamera} from "@react-three/drei";
 
 
 export default function Designer() {
-
     return (<>
         <Canvas camera={cameraProps} gl={glProps}>
             <OrthographicCamera>
                 <DynamicDrawing/>
                 <MapTile imageUrl={'staticmap.png'}/>
                 <SceneLighting/>
-
                 <OrbitControls
                     minPolarAngle={Math.PI / 2} // Restrict top view
                     minAzimuthAngle={0} // Disable tilting to the left
@@ -23,6 +20,5 @@ export default function Designer() {
                 />
             </OrthographicCamera>
         </Canvas>
-        <DesignerOrbitControls/>
     </>)
 }// Designer
